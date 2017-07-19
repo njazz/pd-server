b802d4efccefff53e781428b4f30adba5a743a3e
Alex Nadzharov <anadjarov@gmail.com>
2017-07-19 10:13:20 +0300

    basic tests added

--- tests/pd-server-tests.cpp
+++ tests/pd-server-tests.cpp
@@ -3,4 +3,56 @@
 
 #include "../src/pdServer.hpp"
 
+ServerInstance* testServerInstance;
 
+bool init()
+{
+    return testServerInstance = new ServerInstance();
+}
+
+bool canvas_New()
+{
+    return testServerInstance->createCanvas();
+    ;
+}
+
+bool canvas_NewCopy()
+{
+    ServerCanvas* c = testServerInstance->createCanvas();
+    return new ServerCanvas(c->canvasObject());
+}
+
+bool array_New()
+{
+    ServerCanvas* c = testServerInstance->createCanvas();
+
+    return c->createArray("test-array", 1024);
+    ;
+}
+
+bool object_New()
+{
+    ServerCanvas* c = testServerInstance->createCanvas();
+
+    return !c->createObject("print test-object")->errorBox();
+}
+
+bool object_NewErrorbox()
+{
+    ServerCanvas* c = testServerInstance->createCanvas();
+
+    return c->createObject("not-existing-test-object")->errorBox();
+}
+
+// ------
+
+TEST_CASE("Objects are created", "[objects]")
+{
+    REQUIRE(init());
+
+    REQUIRE(canvas_New());
+    REQUIRE(canvas_NewCopy());
+    REQUIRE(array_New());
+    REQUIRE(object_New());
+    REQUIRE(object_NewErrorbox());
+}
